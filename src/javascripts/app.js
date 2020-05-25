45eab70c1cba743ee56666ad12ccd619e2fdd345
Roman Arestovich <eedoov@gmail.com>
2020-05-25 22:59:20 +0300

    finish

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,13 @@
-import './modules'
+const initMap = () => {
+  // The location of Uluru
+  var uluru = { lat: 47.2558321, lng: 39.6808424 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+};
 
-console.log(`app.js has loaded!`)
+window.initMap = initMap;
